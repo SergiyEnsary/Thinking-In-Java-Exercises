885552b20bc55be64d50fbb00e8c499a2ef924b6
SergiyEnsary <44273382+SergiyEnsary@users.noreply.github.com>
2019-07-24 13:25:04 -0500

    Long Notation

--- Exercise008.java
+++ Exercise008.java
@@ -1,5 +1,12 @@
 package practices;
 
 public class Exercise008 {
-
+  public static void main(String[] args) {
+        long n1 = 0x23fL;    // Hexadecimal (lowercase)
+        System.out.print("n1: " + Long.toBinaryString(n1));
+        long n2 = 0x23FL;    // Hexadecimal (uppercase)
+        System.out.print("n2: " + Long.toBinaryString(n2));
+        long n3 = 01767L;    // Octal
+        System.out.print("n3: " + Long.toBinaryString(n3));
+    }
 }
