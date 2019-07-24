06a5c33e65685357f5a76c463c8532f1d9db5be1
SergiyEnsary <44273382+SergiyEnsary@users.noreply.github.com>
2019-07-24 13:19:47 -0500

    Coin flip exercise

--- Exercise007.java
+++ Exercise007.java
@@ -1,5 +1,23 @@
 package practices;
+import java.util.Random;
 
 public class Exercise007 {
+  private enum Coin{Heads, Tails};
 
+	  Random randomNum = new Random();
+	  private int result;
+	  private int heads = 0;
+	  private int tails = 1;
+	  Coin coinFlip;
+
+	  public void flip(){
+	    result = randomNum.nextInt(2);
+	    if(result == 0){
+	        coinFlip = Coin.Heads;
+	        System.out.println("You flipped Heads!");
+	    }else{
+	        coinFlip = Coin.Tails;
+	        System.out.println("You flipped Tails!");
+	    }
+	}
 }
